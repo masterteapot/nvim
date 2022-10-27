52bf8ca3aee1ae1b367ae48e390305be2bb14aec
masterteapot <71963205+masterteapot@users.noreply.github.com>
2022-10-27 16:59:35 +0400

    Added better LSP support for python and PHP

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -53,8 +53,17 @@ cmp.setup.cmdline(':', {
 })
 
 -- Set up lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+require("nvim-lsp-installer").setup {}
+
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 require('lspconfig')['pyright'].setup {
+  capabilities = capabilities
+}
+require('lspconfig')['intelephense'].setup {
+  capabilities = capabilities
+}
+require('lspconfig')['sumneko_lua'].setup {
   capabilities = capabilities
 }
