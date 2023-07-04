d1b224cc2269bf1a2ae6fe9107f53e6e646c6b30
masterteapot <71963205+masterteapot@users.noreply.github.com>
2023-07-04 13:24:45 +0400

    Cleaned up and removed the vscode

--- init.lua
+++ init.lua
@@ -1,25 +1,22 @@
-if vim.g.vscode then
-    print("Hello vscode")
-else
-	-- Global Options
-	require "user.options"
-	require "user.keymaps"
-	require "user.globals"
-	require "user.autocommands"
-	require "user.colorscheme"
-	require "user.icons"
+-- Global Options
+require "user.options"
+require "user.keymaps"
+require "user.globals"
+require "user.autocommands"
+require "user.colorscheme"
+require "user.colorizer"
+require "user.icons"
 
-	-- Plugins
-	require "user.plugins"
-	require "user.plugins.nvim-tree"
-	require "user.plugins.telescope"
-	require "user.plugins.harpoon"
-	require "user.plugins.comment"
+-- Plugins
+require "user.plugins"
+require "user.plugins.nvim-tree"
+require "user.plugins.telescope"
+require "user.plugins.harpoon"
+require "user.plugins.comment"
 
-	-- LSP type things
-	require "user.lsp"
-	require "user.lsp.cmp"
-	require "user.lsp.dap"
-	require "user.lsp.null-ls"
-	require "user.lsp.treesitter"
-end
+-- LSP type things
+require "user.lsp"
+require "user.lsp.cmp"
+require "user.lsp.dap"
+require "user.lsp.null-ls"
+require "user.lsp.treesitter"
