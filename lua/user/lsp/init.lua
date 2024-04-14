da740d12a74d9f48419e3674347a93abcd2fd637
jared <jared.s.kane@gmail.com>
2024-04-14 21:51:32 +0400

    worked on lsp for go

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -1,6 +1,6 @@
 require("mason").setup()
 require("mason-lspconfig").setup {
-    ensure_installed = { "lua_ls", "cssls", "marksman", "html", "intelephense", "pyright", "jsonls", "eslint" },
+	ensure_installed = { "lua_ls", "cssls", "marksman", "gopls", "html", "intelephense", "pyright", "jsonls", "eslint" },
 }
 local status, lsp = pcall(require, "lspconfig")
 if (not status) then return end
@@ -12,6 +12,16 @@ local lsp_flags = {
 	-- This is the default in Nvim 0.7+
 	debounce_text_changes = 150,
 }
+local c = vim.lsp.protocol.make_client_capabilities()
+c.textDocument.completion.completionItem.snippetSupport = true
+c.textDocument.completion.completionItem.resolveSupport = {
+	properties = {
+		'documentation',
+		'detail',
+		'additionalTextEdits',
+	},
+}
+local capabilities = require("cmp_nvim_lsp").default_capabilities(c)
 
 local on_attach = function(client, bufnr)
 	-- Enable completion triggered by <c-x><c-o>
@@ -19,21 +29,20 @@ local on_attach = function(client, bufnr)
 
 	local bufopts = { noremap = true, silent = false, buffer = bufnr }
 
-	-- code lens 
-    if client.resolved_capabilities.code_lens then
-        local codelens = vim.api.nvim_create_augroup(
-            'LSPCodeLens',
-            { clear = true }
-        )
-        vim.api.nvim_create_autocmd({ 'BufEnter', 'InsertLeave', 'CursorHold' }, {
-            group = codelens,
-            callback = function()
-                vim.lsp.codelens.refresh()
-            end,
-            buffer = bufnr,
-        })
-    end
-
+	-- code lens
+	if client.resolved_capabilities.code_lens then
+		local codelens = vim.api.nvim_create_augroup(
+			'LSPCodeLens',
+			{ clear = true }
+		)
+		vim.api.nvim_create_autocmd({ 'BufEnter', 'InsertLeave', 'CursorHold' }, {
+			group = codelens,
+			callback = function()
+				vim.lsp.codelens.refresh()
+			end,
+			buffer = bufnr,
+		})
+	end
 end
 
 
@@ -50,27 +59,27 @@ require("lspconfig")["intelephense"].setup({
 
 -- Lua
 require("lspconfig")["lua_ls"].setup({
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {'vim'},
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
-		checkThirdParty = "Disable",
-      },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = {
-        enable = false,
-      },
-    },
-  },
+	settings = {
+		Lua = {
+			runtime = {
+				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+				version = 'LuaJIT',
+			},
+			diagnostics = {
+				-- Get the language server to recognize the `vim` global
+				globals = { 'vim' },
+			},
+			workspace = {
+				-- Make the server aware of Neovim runtime files
+				library = vim.api.nvim_get_runtime_file("", true),
+				checkThirdParty = "Disable",
+			},
+			-- Do not send telemetry data containing a randomized but unique identifier
+			telemetry = {
+				enable = false,
+			},
+		},
+	},
 })
 
 require("lspconfig")["eslint"].setup({
@@ -83,56 +92,55 @@ require("lspconfig")["cssls"].setup({
 	flags = lsp_flags,
 })
 
-require("lspconfig")["gopls"].setup({
-	on_attach = on_attach,
-	flags = lsp_flags,
-})
-
-require'lspconfig'.tsserver.setup({
+require 'lspconfig'.tsserver.setup({
 	on_attach = on_attach,
 	flags = lsp_flags,
 	filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
 	cmd = { "typescript-language-server", "--stdio" }
 })
 
-local c = vim.lsp.protocol.make_client_capabilities()
-c.textDocument.completion.completionItem.snippetSupport = true
-c.textDocument.completion.completionItem.resolveSupport = {
-    properties = {
-        'documentation',
-        'detail',
-        'additionalTextEdits',
-    },
-}
-local capabilities = require("cmp_nvim_lsp").default_capabilities(c)
 
 require('lspconfig')['ocamllsp'].setup({
-    cmd = { "ocamllsp" },
-    filetypes = { "ocaml", "ocaml.menhir", "ocaml.interface", "ocaml.ocamllex", "reason", "dune" },
-    root_dir = lsp.util.root_pattern("*.opam", "esy.json", "package.json", ".git", "dune-project", "dune-workspace"),
+	cmd = { "ocamllsp" },
+	filetypes = { "ocaml", "ocaml.menhir", "ocaml.interface", "ocaml.ocamllex", "reason", "dune" },
+	root_dir = lsp.util.root_pattern("*.opam", "esy.json", "package.json", ".git", "dune-project", "dune-workspace"),
 	on_attach = on_attach,
 	capabilities = capabilities
 })
 
--- Python Setup
-local util = require("lspconfig.util")
-local path = util.path
+require("lspconfig")["gopls"].setup({
+	cmd = { "gopls" },
+	filetypes = { "go", "gomod", "gowork", "gotmpl" },
+	root_dir = util.root_pattern("go.mod", "go.work", ".git"),
+	on_attach = on_attach,
+	flags = lsp_flags,
+	capabilities = capabilities,
+	settings = {
+		gopls = {
+			analyses = {
+				unusedparams = true,
+			},
+			staticcheck = true,
+			gofumpt = true,
+		},
+	},
+})
 
 -- Python Setup
 local function get_python_path(workspace)
-  -- Use activated virtualenv.
-  if vim.env.VIRTUAL_ENV then
-    return path.join(vim.env.VIRTUAL_ENV, 'bin', 'python')
-  end
-
-  -- Find and use virtualenv in workspace directory.
-	local match = vim.fn.glob(path.join("/home/jared/.pyenv", "jareds-venv", 'pyvenv.cfg'))
-	if match ~= '' then
-	  return path.join(path.dirname(match), 'bin', 'python')
+	-- Use activated virtualenv.
+	if vim.env.VIRTUAL_ENV then
+		return path.join(vim.env.VIRTUAL_ENV, 'bin', 'python')
 	end
 
-  -- Fallback to system Python.
-  return exepath('python3') or exepath('python') or 'python'
+	-- Find and use virtualenv in workspace directory.
+	local match = vim.fn.glob(path.join("/home/jared/.pyenv", "jareds-venv", 'pyvenv.cfg'))
+	if match ~= '' then
+		return path.join(path.dirname(match), 'bin', 'python')
+	end
+
+	-- Fallback to system Python.
+	return exepath('python3') or exepath('python') or 'python'
 end
 
 require("lspconfig")["pyright"].setup({
@@ -149,7 +157,7 @@ require("lspconfig")["pyright"].setup({
 		}
 	},
 	before_init = function(_, config)
-	config.settings.python.pythonPath = get_python_path(config.root_dir)
+		config.settings.python.pythonPath = get_python_path(config.root_dir)
 	end,
 })
 
--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -5,6 +5,8 @@ local formatting = null_ls.builtins.formatting
 local sources = {
 	formatting.eslint,
 	formatting.black,
+	formatting.gofumpt,
+	formatting.goimports_reviser
 }
 
 null_ls.setup({
