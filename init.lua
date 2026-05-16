fdcb0c16fc409fa011562aea6a8251e2f1d54ceb
Jared Kane <jared.s.kane@gmail.com>
2026-05-16 21:48:03 +0400

    small tweaks

--- init.lua
+++ init.lua
@@ -484,6 +484,7 @@ require('lazy').setup({
       },
 
       completion = {
+        menu = { auto_show = false, auto_show_delay_ms = 500},
         documentation = { auto_show = false, auto_show_delay_ms = 500 },
       },
 
